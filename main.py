55966c1cec88857e856664a4c9507b53b2baf66c
Samuel <xxsephirothxx13@gmail.com>
2023-08-26 21:54:16 -0700

    Assignment 1 percentage

--- main.py
+++ main.py
@@ -11,8 +11,18 @@ def main():
     # Do not delete the return statement
     ########################################
     """
+
+
     return m_perc, f_perc
 
+male = float(input("How many male students: "))
+female = float(input("How many female students: "))
+total = male + female
+m_perc = male / 100
+f_perc = female / 100
+print("Total number of students: ", int(total))
+print("The number of males and females: ", int(male), int(female))
+print(f'The percentages of males and females:\t{m_perc:.2%}, {f_perc:.2%}')
 
 if __name__ == '__main__':
     main()
