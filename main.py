c3525570f0226c6d710a4ced7cb898786cf9420b
Samuel <xxsephirothxx13@gmail.com>
2023-08-26 22:02:50 -0700

    Assignment 1 also error

--- main.py
+++ main.py
@@ -11,10 +11,7 @@ def main():
     # Do not delete the return statement
     ########################################
     """
-
-
     return m_perc, f_perc
-
 male = float(input("How many male students: "))
 female = float(input("How many female students: "))
 total = male + female
@@ -23,6 +20,5 @@ f_perc = female / 100
 print("Total number of students: ", int(total))
 print("The number of males and females: ", int(male), int(female))
 print(f'The percentages of males and females:\t{m_perc:.2%}, {f_perc:.2%}')
-
 if __name__ == '__main__':
     main()
