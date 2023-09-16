7b299a961551b3d271d2731315b3bfd7fad2de95
Samuel <xxsephirothxx13@gmail.com>
2023-09-15 23:10:53 -0700

    redoing my work now

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ def main():
     Use m_perc and f_perc for your results
     ##################################################
     """
+    m_perc = float(input("Number of male students: "))
+    f_perc = float(input("Number of female students: "))
+    total = m_perc + f_perc
+    print("Total number of students: ", int(total))
+    print("The number of males and females: ", int(m_perc), int(f_perc))
+    print(f'The percentages of males and females:\t{m_perc:.2f}, {f_perc:.2f}')
 
     """
     ########################################
@@ -12,11 +18,5 @@ def main():
     ########################################
     """
     return m_perc, f_perc
-m_perc = float(input("Number of male students: "))
-f_perc = float(input("Number of female students: "))
-total = m_perc + f_perc
-print("Total number of students: ", int(total))
-print("The number of males and females: ", int(m_perc), int(f_perc))
-print(f'The percentages of males and females:\t{m_perc:.2f}, {f_perc:.2f}')
 if __name__ == '__main__':
     main()
